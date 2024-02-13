9164d7406544fbca8b11f1e81505b2130e91c758
RexJustesen <rexjustesen1992@gmail.com>
2024-02-13 12:32:00 -0600

    Added Database using sqlite, incorporated data migrations, implemented UsersControllers api

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -11,11 +11,9 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController()
     {
-        _logger = logger;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
