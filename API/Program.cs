8f04a9cf3ea2e99a17c5ed41fec0bde9febe51cf
RexJustesen <rexjustesen1992@gmail.com>
2024-02-13 18:12:48 -0600

    Implemented basic angular frontent

--- API/Program.cs
+++ API/Program.cs
@@ -11,12 +11,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import * as tslib from '../../node_modules/tslib/tslib';
+
+
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
