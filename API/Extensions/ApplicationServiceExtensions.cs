39541c41ab646da33d534717071a0d560890dc32
RexJustesen <rexjustesen1992@gmail.com>
2024-02-19 18:23:50 -0600

    Revert "Added photo upload method in API"
    This reverts commit 729cb6a3571c812caef0276da876ca511a364ada.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using API.Data;
-using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -18,8 +17,6 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-            services.AddScoped<IPhotoService, PhotoService>();
 
             return services;
         }
